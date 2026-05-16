16aa1307f4933ff85bc1de011fd900af3185c913
Arsene-Lontsi <arsene.lontsi10@gmail.com>
2026-05-16 14:58:40 +0100

    initial movie card

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,29 @@
+
+function MovieCard({ movie }) {
+
+    function onFacouriteClick() {
+        console.log("Favourite button clicked for movie:", movie.title);
+        alert(`You have favourited: ${movie.title}`);
+    }
+
+    return (
+        <>
+            <div className="movie-card"> </div>
+            <div className="movie-poster"> 
+                <img src={movie.url} alt={movie.title} />
+                <div className="movie-overlay">
+                    <button className="favourite-btn" onClick={onFacouriteClick}>
+                        ♥
+                    </button>
+                </div>
+            </div>
+
+            <div className="movie-info">
+                <h3>{movie.title}</h3>
+                <p>{movie.release_date}</p>
+            </div>
+        </> 
+    );
+}
+
+export default MovieCard;
